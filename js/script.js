35454a9892c280395f64e7dc463ab0c63e39c7ee
Liliya-993 <notbit4corpse@gmail.com>
2024-11-02 16:41:13 +0200

    mew page

--- js/script.js
+++ js/script.js
@@ -14,3 +14,10 @@ const swiper1 = new Swiper(".swiper", {
         prevEl: ".swiper-button-prev",
     },
 });
+
+document
+    .getElementById("redirectButton")
+    .addEventListener("click", function () {
+        // Перенаправляємо на іншу сторінку
+        window.location.href = "Page-tabl.html";
+    });
