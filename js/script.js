06c298f31e5edca354d33a2dd8f4d9aa552eb08f
Liliya-993 <notbit4corpse@gmail.com>
2025-01-20 09:06:15 +0200

    Update script.js

--- js/script.js
+++ js/script.js
@@ -21,3 +21,16 @@ document
         // Перенаправляємо на іншу сторінку
         window.location.href = "Page-tabl.html";
     });
+
+function calculate() {
+    const constant = 0.0018; // Стале число
+    const inputValue = parseFloat(document.getElementById("inputValue").value);
+
+    if (isNaN(inputValue)) {
+        document.getElementById("resultField").value = "Некоректне число";
+        return;
+    }
+
+    const result = inputValue * constant;
+    document.getElementById("resultField").value = `${result.toFixed(6)} кг`;
+}
