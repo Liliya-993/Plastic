a6fd3be7f4a92666a7be84abcaed1b9b55c0f214
Liliya-993 <notbit4corpse@gmail.com>
2025-01-20 10:06:20 +0200

    Update script.js

--- js/script.js
+++ js/script.js
@@ -24,15 +24,17 @@ document
 
 function calculate() {
     const constant = 0.0018; // Стале число
-    const inputValue = parseFloat(document.getElementById("inputValue").value);
+    const inputValue = parseFloat(document.getElementById('inputValue').value);
 
     if (isNaN(inputValue)) {
-        document.getElementById("resultField").value = "Некоректне число";
+        document.getElementById('resultField').value = 'Некоректне число';
         return;
     }
 
     const result = inputValue * constant;
-  const formattedResult = result.toString();
+
+    // Прибираємо зайві нулі
+    const formattedResult = result.toString();
 
     document.getElementById('resultField').value = `${formattedResult} кг`;
 }
